94b7d1adbf932089c2dd5fce476e3a7d67f01aa7
Anindra123 <bivasdas911@gmail.com>
2024-01-19 17:49:00 +0600

    added resize for right,bottom,bottom-right

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import React, { useRef, useState } from 'react'
 import './App.css'
 import Square from './components/square';
-import { WRAPPER_CONTAINER_HEIGHT, WRAPPER_CONTAINER_WIDTH } from './constants/constants';
+import {
+  INITIAL_BOX_POSITION, INITIAL_PARENT_POSITION,
+  INITIAL_PARENT_SIZE,
+  WINDOW_HEIGHT,
+  WINDOW_WIDTH,
+  WRAPPER_CONTAINER_HEIGHT, WRAPPER_CONTAINER_WIDTH
+} from './constants/constants';
 
-const intial_positon = {
-  x: 250, y: 250
-}
 
-const inital_parent_position = {
-  x: 450,
-  y: 100
-}
 
 function App() {
   const wrapperRef = useRef<HTMLDivElement | null>(null);
-  // const wrapperRect = wrapperRef.current?.getBoundingClientRect();
-  const [divPosition, setDivPosition] = useState(intial_positon);
-  const [parentPostion, setParentPosition] = useState(inital_parent_position)
+  const [divPosition, setDivPosition] = useState(INITIAL_BOX_POSITION);
+  const [parentPostion, setParentPosition] = useState(INITIAL_PARENT_POSITION)
+  const [parentSize, setParentSize] = useState(INITIAL_PARENT_SIZE)
   const [currentPosition, setCurrentPosition] = useState("top");
+  // const [resizePostion, setResizePosition] = useState("right");
   const topBarRef = useRef<HTMLDivElement | null>(null)
+  let resizePostion = "right";
 
 
   let parentStartPosition = { x: 0, y: 0 }
+  let containerSize = { width: 0, height: 0 };
 
   function handleMouseDown(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
 
@@ -49,15 +51,156 @@ function App() {
     document.documentElement.removeEventListener("mouseup", handleMouseUp);
   }
 
+  function handleMouseResizeDown(e: React.PointerEvent<HTMLDivElement>) {
+    containerSize = {
+      width: parentSize.width,
+      height: parentSize.height
+    }
+    resizePostion = e.currentTarget.id;
+
+    document.documentElement.addEventListener("pointermove", handleMouseResizeMove);
+    document.documentElement.addEventListener("pointerup", handleMouseResizeUp);
+  }
+
+  function handleMouseResizeMove(e: PointerEvent) {
+    const temp_size = { ...containerSize }
+    console.log(resizePostion);
+    if (resizePostion === "right") {
+      const newWidth = containerSize.width
+        + (e.clientX - (parentPostion.x + parentSize.width));
+
+      temp_size.width = Math.min(Math.max(newWidth, WRAPPER_CONTAINER_WIDTH), WINDOW_WIDTH);
+    }
+
+    if (resizePostion === "bottom") {
+      const newHeight = containerSize.height +
+        (e.clientY - (parentPostion.y + parentSize.height));
+
+      temp_size.height = Math.min(Math.max(newHeight, WRAPPER_CONTAINER_HEIGHT), WINDOW_HEIGHT);
+    }
+
+    if (resizePostion === "top") {
+      const newHeight = containerSize.height +
+        (e.clientY - (parentPostion.y + parentSize.height));
+
+      temp_size.height = Math.min(Math.max(newHeight, WRAPPER_CONTAINER_HEIGHT), WINDOW_HEIGHT);
+
+    }
+
+    if (resizePostion === "bottom-right") {
+      const newWidth = containerSize.width
+        + (e.clientX - (parentPostion.x + parentSize.width));
+
+      temp_size.width = Math.min(Math.max(newWidth, WRAPPER_CONTAINER_WIDTH), WINDOW_WIDTH);
+      const newHeight = containerSize.height +
+        (e.clientY - (parentPostion.y + parentSize.height));
+
+      temp_size.height = Math.min(Math.max(newHeight, WRAPPER_CONTAINER_HEIGHT), WINDOW_HEIGHT);
+    }
+
+
+
+    setParentSize(temp_size)
+  }
+
+  function handleMouseResizeUp() {
+    document.documentElement.removeEventListener("pointermove", handleMouseResizeMove);
+    document.documentElement.removeEventListener("pointerup", handleMouseResizeUp);
+  }
+
 
   return (
     <>
 
 
+      <div className="wrapper-border" id='right'
+        style={{
+          width: "1px",
+          left: `${parentSize.width + parentPostion.x}px`
+          , top: `${parentPostion.y + 25}px`,
+          height: `${parentSize.height - 50}px`
+        }} onPointerDown={handleMouseResizeDown}>
+
+      </div>
+      <div className='wrapper-border' id='bottom'
+
+        style={{
+          width: `${parentSize.width - 50}px`,
+          top: `${parentSize.height + parentPostion.y}px`,
+          left: `${parentPostion.x + 25}px`,
+          height: "1px",
+        }}
+        onPointerDown={handleMouseResizeDown}
+      >
+      </div>
+      <div className="wrapper-border" id='bottom-right'
+        style={
+          {
+            width: `50px`,
+            height: `50px`,
+            top: `${parentSize.height + parentPostion.y - 51}px`,
+            left: `${parentSize.width + parentPostion.x - 51}px`,
+
+          }} onPointerDown={handleMouseResizeDown}>
+
+      </div>
+      <div className="wrapper-border" id='top-right'
+        style={
+          {
+            width: `50px`,
+            height: `50px`,
+            top: `${parentPostion.y - 1}px`,
+            left: `${parentSize.width + parentPostion.x - 51}px`,
+
+          }} onPointerDown={handleMouseResizeDown}>
+
+      </div>
+      <div className="wrapper-border" id='top-left'
+        style={
+          {
+            width: `50px`,
+            height: `50px`,
+            top: `${parentPostion.y - 1}px`,
+            left: `${parentPostion.x - 1}px`,
+
+          }} onPointerDown={handleMouseResizeDown}>
+
+      </div>
+      <div className="wrapper-border" id='bottom-left'
+        style={
+          {
+            width: `50px`,
+            height: `50px`,
+            top: `${parentSize.height + parentPostion.y - 51}px`,
+            left: `${parentPostion.x - 1}px`,
+
+          }} onPointerDown={handleMouseResizeDown}>
+
+      </div>
+      <div className="wrapper-border" id='top'
+        style={{
+          width: `${parentSize.width - 50}px`,
+          height: "1px",
+          top: `${parentPostion.y - 1}px`,
+          left: `${parentPostion.x + 25}px`,
+
+        }} onPointerDown={handleMouseResizeDown}>
+      </div>
+      <div className="wrapper-border" id='left'
+        style={{
+          width: `1px`,
+          height: `${parentSize.height - 50}px`,
+          top: `${parentPostion.y + 25}px`,
+          left: `${parentPostion.x - 2}px`
+        }} onPointerDown={handleMouseResizeDown}>
+
+      </div>
 
       <div className='wrapper' style={{
         left: `${parentPostion.x}px`
-        , top: `${parentPostion.y}px`
+        , top: `${parentPostion.y}px`,
+        height: `${parentSize.height}px`,
+        width: `${parentSize.width}px`
       }} ref={wrapperRef} >
 
         <Square
@@ -83,6 +226,8 @@ function App() {
           </a>
         </div>
       </div >
+
+
 
 
 
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,3 +2,20 @@ export const WRAPPER_CONTAINER_HEIGHT = 604;
 export const WRAPPER_CONTAINER_WIDTH = 604;
 export const SMALL_BOX_HEIGHT = 104;
 export const SMALL_BOX_WIDTH = 104;
+export const INITIAL_BOX_POSITION = {
+  x: 250,
+  y: 250,
+};
+
+export const INITIAL_PARENT_POSITION = {
+  x: 450,
+  y: 100,
+};
+
+export const INITIAL_PARENT_SIZE = {
+  width: 600,
+  height: 600,
+};
+
+export const WINDOW_WIDTH = document.documentElement.clientWidth;
+export const WINDOW_HEIGHT = document.documentElement.clientHeight;
